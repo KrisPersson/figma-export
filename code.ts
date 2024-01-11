a0e8b631e4cb2690abe84ab453d15a44b82755dd
KrisPersson <112572136+KrisPersson@users.noreply.github.com>
2024-01-11 19:02:12 +0100

    percentage to rgb function

--- code.ts
+++ code.ts
@@ -1,10 +1,23 @@
+function convertPercentageToRgb(rgbObject: { r: number, g: number, b: number, a: number  }) {
+  const { r, g, b, a } = rgbObject;
+  const rgbScaleLength = 255;
+  const convertedObject = {
+      r: r * rgbScaleLength,
+      g: g * rgbScaleLength,
+      b: b * rgbScaleLength,
+      a: a * rgbScaleLength
+  };
+  return convertedObject;
+}
 
 figma.showUI(__html__);
-figma.ui.resize(500, 500)
+figma.ui.resize(500, 300)
 figma.ui.onmessage = msg => {
+
+  console.log(convertPercentageToRgb({ r: 0.9176470637321472, g: 0.34117648005485535, b: 0.4313725531101227, a: 1  }))
   
-  const nodes: SceneNode[] = [];
-  const localCollections = figma.variables.getLocalVariableCollections();
+  // const nodes: SceneNode[] = [];
+  // const localCollections = figma.variables.getLocalVariableCollections();
 
   const localVariables = msg.type !== 'all' ? figma.variables.getLocalVariables(msg.type.toUpperCase()) : figma.variables.getLocalVariables(); // argument optional to filter by variabel-type, for example "STRING"
 
