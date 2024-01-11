6fdd8886e27b542ae0b4d830a06d875cb1755efe
KrisPersson <112572136+KrisPersson@users.noreply.github.com>
2024-01-11 13:44:11 +0100

    console-logging variables, all or by type

--- code.ts
+++ code.ts
@@ -1,16 +1,14 @@
 
 figma.showUI(__html__);
-
+figma.ui.resize(500, 500)
 figma.ui.onmessage = msg => {
   
-  if (msg.type === 'all') {
-    const nodes: SceneNode[] = [];
-    const localCollections = figma.variables.getLocalVariableCollections();
-    const localVariables = figma.variables.getLocalVariables('COLOR'); // argument optional to filter by variabel-type, for example "STRING"
+  const nodes: SceneNode[] = [];
+  const localCollections = figma.variables.getLocalVariableCollections();
 
-    console.log(localVariables)
+  const localVariables = msg.type !== 'all' ? figma.variables.getLocalVariables(msg.type.toUpperCase()) : figma.variables.getLocalVariables(); // argument optional to filter by variabel-type, for example "STRING"
+
+  console.log(localVariables)
     
-  }
-
-  figma.closePlugin();
+  // figma.closePlugin();
 };
