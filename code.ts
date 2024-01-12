9a02aa315db38843a0c0785a88f1470a50724451
KrisPersson <112572136+KrisPersson@users.noreply.github.com>
2024-01-12 09:46:56 +0100

    parsing new objects with label, name, and rgba-object with parsed values

--- code.ts
+++ code.ts
@@ -1,11 +1,11 @@
-function convertPercentageToRgb(rgbObject: { r: number, g: number, b: number, a: number  }) {
+function convertPercentageToRgb(rgbObject: RGBA) {
   const { r, g, b, a } = rgbObject;
   const rgbScaleLength = 255;
   const convertedObject = {
-      r: r * rgbScaleLength,
-      g: g * rgbScaleLength,
-      b: b * rgbScaleLength,
-      a: a * rgbScaleLength
+      r: Math.floor(r * rgbScaleLength),
+      g: Math.floor(g * rgbScaleLength),
+      b: Math.floor(b * rgbScaleLength),
+      a: a 
   };
   return convertedObject;
 }
@@ -14,14 +14,23 @@ figma.showUI(__html__);
 figma.ui.resize(500, 300)
 figma.ui.onmessage = msg => {
 
-  console.log(convertPercentageToRgb({ r: 0.9176470637321472, g: 0.34117648005485535, b: 0.4313725531101227, a: 1  }))
   
   // const nodes: SceneNode[] = [];
   // const localCollections = figma.variables.getLocalVariableCollections();
 
   const localVariables = msg.type !== 'all' ? figma.variables.getLocalVariables(msg.type.toUpperCase()) : figma.variables.getLocalVariables(); // argument optional to filter by variabel-type, for example "STRING"
+  console.log(localVariables[0].name.split('/'))
 
-  console.log(localVariables)
+  const parsedColorObjects = localVariables.map(variable => {
+    const identifier = '1:0'
+    const groupAndColorName = variable.name.split('/')
+    return {
+      label: groupAndColorName[0],
+      name: groupAndColorName[groupAndColorName.length - 1],
+      rgba: convertPercentageToRgb(variable.valuesByMode[identifier] as RGBA) 
+    }
+  })
+  console.log(parsedColorObjects)
     
   // figma.closePlugin();
 };
