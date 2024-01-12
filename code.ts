0fa1a218658fc49c7a6a4989d39e3b66ffeb4c63
KrisPersson <112572136+KrisPersson@users.noreply.github.com>
2024-01-12 15:24:38 +0100

    basic output: CSS-string

--- code.ts
+++ code.ts
@@ -1,3 +1,5 @@
+// import clipboard from 'clipboardy';
+
 function convertPercentageToRgb(rgbObject: RGBA) {
   const { r, g, b, a } = rgbObject;
   const rgbScaleLength = 255;
@@ -18,19 +20,35 @@ figma.ui.onmessage = msg => {
   // const nodes: SceneNode[] = [];
   // const localCollections = figma.variables.getLocalVariableCollections();
 
-  const localVariables = msg.type !== 'all' ? figma.variables.getLocalVariables(msg.type.toUpperCase()) : figma.variables.getLocalVariables(); // argument optional to filter by variabel-type, for example "STRING"
-  console.log(localVariables[0].name.split('/'))
+  const localVariables = msg.type !== 'all' ? 
+    figma.variables.getLocalVariables(msg.type.toUpperCase()) :
+    figma.variables.getLocalVariables(); // argument optional to filter by variabel-type, for example "STRING"
+
+
 
   const parsedColorObjects = localVariables.map(variable => {
     const identifier = '1:0'
     const groupAndColorName = variable.name.split('/')
     return {
-      label: groupAndColorName[0],
+      group: groupAndColorName[0],
       name: groupAndColorName[groupAndColorName.length - 1],
       rgba: convertPercentageToRgb(variable.valuesByMode[identifier] as RGBA) 
     }
   })
-  console.log(parsedColorObjects)
+
+  const cssString: string = parsedColorObjects.reduce((acc: string, cur: { group: string, name: string, rgba: RGBA }) => {
+    const label = cur.name.split(' ')[0].toLowerCase()
+    const weight = cur.name.split(' ').find(item => { 
+      return item === '0' ? true : !!Number(item)
+    })
+    const {r, g, b, a} = cur.rgba
+    return acc + `--_palette-${label}-${weight}: rgba(${r}, ${g}, ${b}, ${a});\n`
+  }, '/* Palette */ \n')
+
+  // clipboard.writeSync(cssString)
+  console.log(cssString)
     
-  // figma.closePlugin();
+  figma.closePlugin();
 };
+
+
